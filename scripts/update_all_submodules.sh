72cc547638c49a9714cb6e74f5126719dbfebf0b
houssem <hbenali@exoplatform.com>
2020-03-29 13:11:27 +0200

    Add update_all_submodules.sh

---
+++ scripts/update_all_submodules.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+git submodule foreach "git checkout develop || git checkout master; git pull --all"
+if git diff-index --quiet HEAD; then
+   git commit -am "Daily update: $(date +'%d-%M-%Y')" && git push 
+fi
